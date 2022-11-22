880605f3b6204c7350a8a0faef45b02bd3872a00
BeatrizAimee <86250587+BeatrizAimee@users.noreply.github.com>
2022-11-22 14:00:42 -0300

    mudanças busca sequencial

--- include/funcoes_de_busca.h
+++ include/funcoes_de_busca.h
@@ -3,11 +3,15 @@
 
 #include "topologiaRede.h"
 #include "funcoes_fornecidas.h"
+#include "funcoes_de_escrita.h"
+#include "funcoes_de_leitura.h"
 
 //compara inteiros
 int compara_campo_inteiro(int, int, reg_dados*);
+int busca_inteiro(reg_dados*, FILE*, int, int*, int);
 
 //compara strings
 int compara_campo_string(int, char*, reg_dados* );
+int busca_string(reg_dados*, FILE*, int, int*, char*);
 
 #endif
