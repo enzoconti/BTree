732377e2880acf8eb6c0a3b47ebc3f48dc463578
enzoconti <enzo.conti@usp.br>
2022-11-24 00:12:32 -0300

    improving comments specially on insert functions

--- Makefile
+++ Makefile
@@ -1,6 +1,6 @@
 CC = gcc
 MAIN = src/main.c
-AUXFILES = src/btree.c src/btree_rw_e_io.c src/funcoes_de_abertura.c src/funcoes_de_busca.c src/funcoes_de_escrita.c src/funcoes_de_leitura.c src/funcionalidades.c src/funcoes_auxiliares.c src/funcoes_checagem.c src/funcoes_fornecidas.c src/topologiaRede.c
+AUXFILES = src/btree.c src/btree_rw_e_io.c src/funcoes_de_abertura.c src/funcoes_de_busca.c src/funcoes_de_escrita.c src/funcoes_de_leitura.c src/funcionalidades.c src/funcoes_auxiliares.c src/funcoes_checagem.c src/funcoes_fornecidas.c src/topologiaRede.c src/funcoes_de_print.c
 EXECNAME = main
 
 all:
@@ -8,3 +8,4 @@ all:
 
 run:
 	./${EXECNAME}
+
--- src/btree.c
+++ src/btree.c
@@ -108,15 +108,15 @@ void busca_indexada(int valor, int* num_registros_encontrados, reg_cabecalho_arv
     }
 }
 
+// funcao master que chama a recursao da insercao da btree
 void insercao_btree(FILE*fp, reg_cabecalho_arvore*h, int key, int data_rrn_4insertion){
     int flag_retorno;
     int promoted_child, promoted_key, promoted_data_rrn;
-    reg_dados_indice* new_root;
-    reg_dados_indice* reg_buscado = cria_registro_dados_indice();
-    int found_pos, flag_busca, num_paginas_lidas;
+    reg_dados_indice* new_root; // em possivel split na raiz
+    int flag_busca;
 
     //debug//printf("calling _insercao_btree first recursion\n");
-    flag_retorno = _insercao_btree(fp,h,h->noRaiz,key,data_rrn_4insertion,&promoted_child,&promoted_key,&promoted_data_rrn);
+    flag_retorno = _insercao_btree(fp,h,h->noRaiz,key,data_rrn_4insertion,&promoted_child,&promoted_key,&promoted_data_rrn); // chama a primeira recursao
     //debug//printf("flag_retorno has returned _insercao_btree as %d\n",flag_retorno);
 
     if(flag_retorno == PROMOTION){ // houve split na raiz ou a arvore estava vazia
@@ -127,7 +127,7 @@ void insercao_btree(FILE*fp, reg_cabecalho_arvore*h, int key, int data_rrn_4inse
         // se houve split na raiz(e nao estava vazia), entao o no raiz != -1 e portanto o no criado nao eh folha
         if(h->noRaiz != -1){
         new_root->folha[0] = '0';
-        }
+        } // em caso contrario o no eh folha nao o alteramos ja que o padrao de cria_registro_dados_indice() eh inicializar com folha[0] = 1
 
         new_root->alturaNo = h->alturaArvore + 1;
 
@@ -149,9 +149,9 @@ void insercao_btree(FILE*fp, reg_cabecalho_arvore*h, int key, int data_rrn_4inse
         //debug//printa_registro_arvore(new_root);
         //debug//printf("header updated to:\n");
         //debug//printHeaderArvore(h);
-        escrever_dados_indice_porRRN(fp, new_root->RRNdoNo,new_root); // CRIAR ESSA FUNCAO
+        escrever_dados_indice_porRRN(fp, new_root->RRNdoNo,new_root);
     }
-    h->nroChavesTotal++;
+    h->nroChavesTotal++; // unica atualizacao feita independentemente de ter havido split na raiz
 
 }
 
@@ -161,7 +161,7 @@ void insercao_btree(FILE*fp, reg_cabecalho_arvore*h, int key, int data_rrn_4inse
  * 
  * @param fp arquivo de indice onde esta a btree
  * @param h cabecalho do arquivo ja lido
- * @param current_index_rrn rrn no arquivo de indice da pagina da btree em que queremos inserir
+ * @param rrn_indice_atual rrn no arquivo de indice da pagina da btree em que queremos inserir
  * @param key chave a ser inserida
  * @param data_rrn_4insertion rrn no arquivo de dados que deve ser inserido
  * @param promoted_child rrn do filho promovido daqui pro nivel superior
@@ -180,9 +180,11 @@ int _insercao_btree(FILE* fp,reg_cabecalho_arvore* h, int rrn_indice_atual, int 
         return PROMOTION; // promovemos a key para a pagina pai na qual sera inserida
     }
 
-
+    // criamos um novo reg que eh o reg atual correspondente ao rrn_indice_atual passado como argumento da funcao
     reg_dados_indice *reg_arvore_atual = cria_registro_dados_indice();
     ler_dados_indice_porRRN(fp, rrn_indice_atual, reg_arvore_atual);
+    
+    // buscamos na pagina a posicao em que a chave esta ou deveria estar
     flag_insercao = busca_na_pagina(key, &posicao_key, reg_arvore_atual);
         
     if(flag_insercao == ENCONTRADO) return INSERTION_ERROR; // retorna erro se ja possui a chave
@@ -192,10 +194,11 @@ int _insercao_btree(FILE* fp,reg_cabecalho_arvore* h, int rrn_indice_atual, int 
     int* promoted_below_key = malloc(sizeof(int));
     int* promoted_below_data_rrn = malloc(sizeof(int));
 
+    // chamada recursiva para onde a chave deveria estar (nao esta pois flag_insercao == ENCONTRADO ja foi descartado)
     flag_retorno =  _insercao_btree(fp,h, reg_arvore_atual->ponteiroSubarvore[posicao_key] ,key,data_rrn_4insertion, promoted_below_child, promoted_below_key, promoted_below_data_rrn);
 
 
-    if(flag_retorno == NO_PROMOTION || flag_retorno == INSERTION_ERROR){
+    if(flag_retorno == NO_PROMOTION || flag_retorno == INSERTION_ERROR){ // houve erro ou a inserco ja ocorreu na camada de baixo e esse nivel nao precisa realizar mais nada
         return flag_retorno;
     }
     // flag_retorno == PROMOTION a partir daqui, pois NO_PROMOTION e INSERTION_ERROR foram descartados
@@ -203,24 +206,25 @@ int _insercao_btree(FILE* fp,reg_cabecalho_arvore* h, int rrn_indice_atual, int 
         //debug//printf("inside has_space case and inserting on reg_arvore atual as:\n");
         //debug//printa_registro_arvore(reg_arvore_atual);
         //debug//printf("and the values for insertion are: key=%d,rrn=%d,child=%d", *promoted_below_key, *promoted_below_data_rrn, *promoted_below_child);
-        flag_insercao = insere_na_pagina(reg_arvore_atual,*promoted_below_key,*promoted_below_data_rrn,*promoted_below_child);
+        flag_insercao = insere_na_pagina(reg_arvore_atual,*promoted_below_key,*promoted_below_data_rrn,*promoted_below_child); // simplesmente inserimos ordenadametne na pagina
         //debug//printf("going to write on file on rrn %d with reg_arvore_atual as:\n", reg_arvore_atual->RRNdoNo);
         //debug//printa_registro_arvore(reg_arvore_atual);
-        escrever_dados_indice_porRRN(fp,reg_arvore_atual->RRNdoNo,reg_arvore_atual); // escreve os dados atualizados da pagina // CRIAR ESSA FUNCAO
+        escrever_dados_indice_porRRN(fp,reg_arvore_atual->RRNdoNo,reg_arvore_atual); // escreve os dados atualizados da pagina 
         
 
-        if(flag_insercao == ENCONTRADO) return INSERTION_ERROR;
+        if(flag_insercao == ENCONTRADO) return INSERTION_ERROR; // descartamos tambem o erro caso o insere_na_pagina encontre a chave onde for inserir
         else return NO_PROMOTION;
-    }else{
+    }else{ // nao temos espaco e devemos dar split com uma nova pagina
         // cria nova pagina
         reg_dados_indice *newreg_arvore;
         newreg_arvore = cria_registro_dados_indice();
-        newreg_arvore->RRNdoNo = h->RRNproxNo;
+        newreg_arvore->RRNdoNo = h->RRNproxNo; // escreveremos no prox RRN disponivel
         h->RRNproxNo++;
-        strcpy(newreg_arvore->folha,reg_arvore_atual->folha);
-        newreg_arvore->alturaNo = reg_arvore_atual->alturaNo;
+        strcpy(newreg_arvore->folha,reg_arvore_atual->folha); // propriedade de folha eh herdada da pagina "irma"
+        newreg_arvore->alturaNo = reg_arvore_atual->alturaNo; // altura do no tambem eh herdada da pagina "irma"
 
         //debug//printf("############################################################\n\t\t\tsplit\t\t\n############################################################\n");
+        // rotina de split separa o conjunto de chaves que estavam na pagina atual + a chave inserida e promove uma
         split(*promoted_below_key,*promoted_below_data_rrn,*promoted_below_child,promoted_key,promoted_data_rrn,promoted_child,reg_arvore_atual,newreg_arvore);
 
         // reescreve os dados de ambas as paginas da btree
@@ -231,16 +235,42 @@ int _insercao_btree(FILE* fp,reg_cabecalho_arvore* h, int rrn_indice_atual, int 
     }
     
 }
+
+   /**
+    * @brief uma funcao que divide o mais igualmente possivel o conjunto de chaves formado pelas chaves em um no cheio + a chave a ser inserida em dois nos da arvore b
+    * 
+    * @param key chave a ser inserida
+    * @param data_rrn rrn no arquivo de dados que corresponde a chave a ser inserida
+    * @param right_child rrn do filho da direita da chave a ser inserida
+    * @param upkey chave a ser promovida
+    * @param updata_rrn rrn no arquivo de dados que corresponde a chave a ser promovida
+    * @param upchild filho da direita a ser promovido
+    * @param r pagina original da arvore b
+    * @param newr pagina recem criada da arvore b
+    */
     /* 
+        explicacao do funcionamento da funcao split:
         um array auxiliar aux[TAM+1] recebe o array original a[TAM] + o valor pra insercao 
-        [a1 , a2 , ... , an] + valor_insercao = [aux1, aux2, ..., aux(n+1) ]
+        [a0 , a1 , ... , a(n-1)] + valor_insercao = [aux0, aux1, ..., aux(n) ]
         sendo que essa insercao eh feita pelo algoritmo de insercao ordenada similar ao da funcao insere_na_pagina
         nesse caso, como a ordem eh impar, teremos a seguinte construcao pros arrays de chave e de data_rrn:
-        [a1, a2, a3, a4] + valor_insercao = [aux1, aux2, aux3, aux4, aux5]
-        assim, promovemos a chave aux3 para distribuir mais igualmente as chaves, isto eh, [aux1,aux2] ficam na pagina original e [aux4,aux5] na nova pagina
-        para o array de subarvores filhas, temos que a insercao so ocorre em nos folha
-        logo, todos filhos sao -1 na pagina original e na nova pagina
-        e o filho promovido eh o rrn da nova pagina(rrn_np no esquema)
+        [a0, a1, a2, a3] + valor_insercao = [aux0, aux1, aux2, aux3, aux4]
+        assim, promovemos a chave aux2 para distribuir mais igualmente as chaves, isto eh, [aux0,aux1] ficam na pagina original e [aux3,aux4] na nova pagina
+
+        apesar da insercao ocorrer apenas em nos folha, o split pode ocorrer em nos nao folha por potencialmente se propagar pra cima
+        portanto devemos nos preocupar tambem com o array de ponteiros para subarvores filhas, no qual temos a seguinte construcao:
+        [auxf0, auxf1, ..., auxf(n+1)] = [f0, f1, ..., f(n)] + filho_insercao
+        nesse caso, temos n valores para o array original de filhos pois tem-se um filho a mais do que o numero de chaves (assim como em uma arvore binaria com apenas uma chave tem-se dois filhos)
+        dessa forma, o que acontece eh que cada chave[i] carrega, alem de seu data_rrn[i], seu filho da direita[i+1]
+        com isso, fica mais facil trabalhar com um array apenas com os filhos da direita, excluindo o filho mais a esquerda do no original(ele nao sofre alteracoes durante todo split)
+        portanto, o array aux_right_children tem todos filhos da direita das chaves originais mais o que deve ser inserido
+        dessa forma, o array aux_right_child eh manipulado exatamente como o array data_rrn, isto eh, eh apenas preenchido de acordo com a ordenacao de aux_keys
+
+        no entanto, nesse caso, a promocao leva o proprio rrn do no criado(rrn_np no esquema),
+        uma vez que todas keys [ aux_keys(posicao_promocao+1), ... , aux_keys(n)] soa necessariamente maiores que a key promovida aux_keys(posicao_promocao) pela ordenacao do array
+        com isso, o filho da direita da chave a ser inserida, isto eh, aux_right_children[posicao_promocao] eh inserido na esquerda do no criado
+        assim, os dois filhos mais a esquerda do no criado sao os casos especiais: o do no original(mais a esquerda) nao se altera, e o do novo no(mais a direita) recebe o filho da direita da chave que eh promovida
+        no esquema abaixo, para economizar espaco, o array aux_right_children eh nomeado apenas de arc
 
         assim teremos a construcao:
 
@@ -248,14 +278,18 @@ int _insercao_btree(FILE* fp,reg_cabecalho_arvore* h, int rrn_indice_atual, int 
             [...,        chavedecimai,     chavedecima(i+1), ...]    
                    /                    \              \
                   /   promocao:  ^       \             ...
-                 /    chave3     |        \
-                /   data_rrn3    |         \
+                 /    chave2     |        \
+                /   data_rrn2    |         \
                /    rrn_np       |          \
               /                  |           \
             pagina original                nova pagina
-            [chave1,chave2]               [chave4,chave5]
+            [chave0,chave1]               [chave3,chave4]
            /       |       \            /        |      \
-        [NIL,      NIL,      NIL]     [NIL,      NIL,    NIL]
+        [f0,      arc0,      arc1]     [arc2,      arc3,    arc4]
+                                        | |
+                                        | |__|\
+                                        |____  | aux_right_childen[2] eh o filho da direita da chave2, a chave a ser promovida
+                                             |/
 
         apesar de no esquema assumirmos ORDEM_ARVORE_B como impar(i.e numero de chave eh par), isso nao faz diferenca no algoritmo
         caso tenhamos 4 chaves(ordem 5) + 1 de insercao e devemos promover uma, promovemos a chave3(posicao 2)
@@ -274,8 +308,11 @@ void split(int key, int data_rrn,int right_child, int* upkey, int* updata_rrn, i
     int has_added_value=0; // indica se o valor ja foi adicionado
     int j=0;              // contador adicional que percorre chaveBusca[] e ponteiroSubarvore[]
     for(int i=0;i<ORDEM_ARVORE_B;i++){
-        if( (j == ORDEM_ARVORE_B-1) || (has_added_value == 0 && key < r->chaveBusca[j]) ){ // se o valor ainda nao foi adicionado e encontramos a primeira chave em chaveBusca[] maior que a key, inserimos a key ao inves dessa chaveBusca[]
-            has_added_value = 1;
+        // se o valor ainda nao foi adicionado e encontramos a primeira chave em chaveBusca[] maior que a key 
+        // OU se ja percorremos todas chaves originais(caso em que a chave a ser inserida eh a maior)
+        // inserimos a key ao inves dessa chaveBusca[]
+        has_added_value = 1;
+        if( (j == ORDEM_ARVORE_B-1) || (has_added_value == 0 && key < r->chaveBusca[j]) ){ 
             aux_keys[i] = key;
             aux_data_rrns[i] = data_rrn;
             aux_right_children[i] = right_child;
@@ -321,13 +358,15 @@ void split(int key, int data_rrn,int right_child, int* upkey, int* updata_rrn, i
         newr->chaveBusca[i] = aux_keys[i+promotion_position+1];
         newr->ponteiroSubarvore[i+1] = aux_right_children[i+promotion_position+1];
     }
+    // o fiho da direita da chave a ser promovida fica na posicao mais a esquerda do novo no
     newr->ponteiroSubarvore[0] = aux_right_children[promotion_position];
 
+    // atualizando o numero de chaves em cada no
     newr->nroChavesNo = promotion_position;
     r->nroChavesNo = promotion_position;
     if(ORDEM_ARVORE_B % 2 == 0) r->nroChavesNo++; // o da esquerda fica com um a mais se a ordem for par
 
-    // colocando NIL nos arrays com valores invalidos
+    // colocando NIL nos arrays com valores invalidos para padronizacao de lixo
     for(int i=promotion_position;i<ORDEM_ARVORE_B-1;i++){
         r->RRNdoRegistro[i] = -1;
         r->chaveBusca[i] = -1;
@@ -357,13 +396,14 @@ int busca_na_pagina(int key, int* pos, reg_dados_indice* r){
     }
 
     if(i == r->nroChavesNo){ // isso significa que nao encontrou nenhuma chave maior que ela
-        *pos = i; // deveria estar por ultimo (no caso de a pagina ja estar cheia, isso estara fora dos array bounds e indica pagina cheia  )
+        *pos = i; // deveria estar por ultimo (isso sai dos array bounds do array original, mas pode ser usado no array de ponteirosSubarvores ja que tem um a mais de tamanho)
         return NAO_ENCONTRADO;
     }
 
     return SEARCH_ERROR;
 }
 
+// insere ordenadamente a key insert_key com seu dado insert_data_rrn e seu filho da direita insert_child no reg_dados_indice* r
 int insere_na_pagina(reg_dados_indice* r,int insert_key, int insert_data_rrn, int insert_child){
     int pos,flag_insercao; // posicao onde sera inserido no array ordenado
     
